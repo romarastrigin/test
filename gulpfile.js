f4b907714c6821a361470081404418cdacdfea3a
romarastrigin <rastrigin05@gmail.com>
2022-09-09 19:18:55 +0300

    added section download

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,8 @@ function scripts() {
     return src([
             "node_modules/jquery/dist/jquery.js",
             "node_modules/mixitup/dist/mixitup.js",
-            "app/js/main.js",
+            "node_modules/slick-carousel/slick/slick.js",
+            "app/js/main.js"
         ])
         .pipe(concat("main.min.js"))
         .pipe(uglify())
